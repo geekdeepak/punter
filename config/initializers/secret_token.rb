e66e4fd8ace15318a51acd746a5b2f9f40ea3d3a
Deepak Sharma <geekdeepak@gmail.com>
2012-04-14 01:59:23 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punter::Application.config.secret_token = '6943cd7a9d504fad2f7d26ecc8a75c92c416b8c344ce61bb831889cf99a57d51455115529613d83a2ecf1b2c0413737212e1cb714a385f0c1b5940577362ee2d'
